a3a4928ce043749fc57825fcb0b5a58c1b55ac87
7efm <38299622+7efm@users.noreply.github.com>
2018-11-30 13:50:30 -0800

    PixLab
    -On A4

---
+++ classes/Answers.java
@@ -0,0 +1,27 @@
+/**
+ * Write a description of class Answers here.
+ *
+ * @author (Jacinto Garcia)
+ * @version (11.29.18)
+ */
+// A1:
+// Q1) It takes 8 bits to represent values from 0 to 255.
+// Q2) It takes 3 bytes to represent a color in the RGB color model.
+// Q3) There are 307,200 pixels in the picture.
+
+// A2:
+// Q1) You can make pink by setting the red slider to 255 and keeping blue a higher value than green.
+// Q2) Yellow can be made by setting the red slider to 255 and Setting the Green slider to 255.
+// Q3) Purple can be made by setting the Blue and Red sliders equal to eachother and having the green slider be a low number.
+// Q4) White is made by setting all the sliders to 255.
+
+// A3: 
+// Q1) The row index for the top left is 0.
+// Q2) The column index is 0.
+// Q3) The rightmost column index is 639
+// Q4) The bottommost index for the row is 479.
+// Q5) It increases from top to bottom.
+// Q6) Column increases from left to right.
+
+
+
